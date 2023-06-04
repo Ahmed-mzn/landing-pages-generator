e60ca3e3d8c6d254a14be6654f925e97bfc23066
Ahmed <ahmedzeini3200@gmail.com>
2023-06-04 17:38:51 +0100

    fix js scripts errors

--- lading_pages_generator/settings.py
+++ lading_pages_generator/settings.py
@@ -164,11 +164,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_ROOT = '/home/khaled/static/'
+# STATIC_ROOT = '/home/khaled/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
