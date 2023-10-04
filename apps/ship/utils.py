076b01ed1ba675153bcebb5037b62fd4f51db6f1
Ahmed <ahmedzeini3200@gmail.com>
2023-10-04 19:07:13 +0100

    add whatsapp message for test(2)

--- apps/ship/utils.py
+++ apps/ship/utils.py
@@ -54,98 +54,47 @@ def create_ship(order):
         headers = {
             'Authorization': 'Bearer EAAC4eE8WAOkBO1NjrlEWyaHhRUrj5uyZBx5a2asZCQKo9ht8EeksJhvzmKi2MYaohIgIXMjaODijpvT6iiphXo1ZCL6NNhLDakfmwZC4vEGxZBZC68sF5VNS0xQgJPcWdYEh4AyADWsVwkClNsiaunzDRUBCJX0q43TJcy72ik8ZBIAV8ZA5kdUB9Em3AqdnyKsw81fltinyNeYMzH8Q'
         }
-        if order.payment_type == 'cod':
-            data = {
-                "messaging_product": "whatsapp",
-                "recipient_type": "individual",
-                "to": "22220004200",
-                "type": "template",
-                "template": {
-                    "name": "cod_1689956899",
-                    "language": {
-                        "code": "ar"
+        data = {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": "22220004200",
+            "type": "template",
+            "template": {
+                "name": "indelivery_1688226612",
+                "language": {
+                    "code": "ar"
+                },
+                "components": [
+                    {
+                        "type": "header",
+                        "parameters": [
+                            {
+                                "type": "image",
+                                "image": {
+                                    "link": "https://b.top4top.io/p_2738i6gqn1.jpg"
+                                }
+                            }
+                        ]
                     },
-                    "components": [
-                        {
-                            "type": "header",
-                            "parameters": [
-                                {
-                                    "type": "image",
-                                    "image": {
-                                        "link": "https://i.ibb.co/vQFPLpZ/4-2.png"
-                                    }
-                                }
-                            ]
-                        },
-                        {
-                            "type": "body",
-                            "parameters": [
-                                {
-                                    "type": "text",
-                                    "text": order.lead.name
-                                },
-                                {
-                                    "type": "text",
-                                    "text": f'SFT{order.id}'
-                                },
-                                {
-                                    "type": "text",
-                                    "text": order.amount
-                                },
-                                {
-                                    "type": "text",
-                                    "text": order.lead.address
-                                }
-                            ]
-                        }
-                    ]
-                }
+                    {
+                        "type": "body",
+                        "parameters": [
+                            {
+                                "type": "text",
+                                "text": order.lead.name
+                            },
+                            {
+                                "type": "text",
+                                "text": "https://aymakan.com.sa/en/tracking/173926"
+                            }
+                        ]
+                    }
+                ]
             }
-            res = requests.post("https://graph.facebook.com/v14.0/109351868894671/messages", headers=headers, json=data)
-            print(res)
-            print(res.text)
-        else:
-            data = {
-                "messaging_product": "whatsapp",
-                "recipient_type": "individual",
-                "to": "22220004200",
-                "type": "template",
-                "template": {
-                    "name": "indelivery_1688226612",
-                    "language": {
-                        "code": "ar"
-                    },
-                    "components": [
-                        {
-                            "type": "header",
-                            "parameters": [
-                                {
-                                    "type": "image",
-                                    "image": {
-                                        "link": "https://b.top4top.io/p_2738i6gqn1.jpg"
-                                    }
-                                }
-                            ]
-                        },
-                        {
-                            "type": "body",
-                            "parameters": [
-                                {
-                                    "type": "text",
-                                    "text": order.lead.name
-                                },
-                                {
-                                    "type": "text",
-                                    "text": "https://aymakan.com.sa/en/tracking/173926"
-                                }
-                            ]
-                        }
-                    ]
-                }
-            }
-            res = requests.post("https://graph.facebook.com/v14.0/109351868894671/messages", headers=headers, json=data)
-            print(res)
-            print(res.text)
+        }
+        res = requests.post("https://graph.facebook.com/v14.0/109351868894671/messages", headers=headers, json=data)
+        print(res)
+        print(res.text)
 
         print('[+] Finish ship creation')
         return is_shipped
